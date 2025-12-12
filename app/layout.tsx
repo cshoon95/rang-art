2e1070492e0ec31774d0837ab62ee1b12639ed26
조수훈 <cshoon80@gmail.com>
2025-12-12 14:53:35 +0900

    splash 작업

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,20 +12,20 @@ import AuthCheck from "@/components/auth/AuthCheck";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "RANG ART - 아이들의 행복한 창작놀이터", // 사이트 제목
-  description: "선생님과 원장님을 위한 올인원 미술학원 관리 플랫폼", // 사이트 설명
+  title: "랑아트 미술학원 😊", // 사이트 제목
+  description: "랑아트 미술학원만을 위한 올인원 관리 플랫폼", // 사이트 설명
   icons: {
-    icon: "/icon.png", // 파비콘 설정
-    apple: "/icon.png", // 애플 터치 아이콘 설정 (선택 사항)
+    icon: "https://cshoon95-rang-art.vercel.app/icon.png", // 파비콘 설정
+    apple: "https://cshoon95-rang-art.vercel.app/icon.png", // 애플 터치 아이콘 설정 (선택 사항)
   },
   openGraph: {
-    title: "RANG ART - 아이들의 행복한 창작놀이터", // 카톡 등 SNS 공유 시 제목
-    description: "선생님과 원장님을 위한 올인원 미술학원 관리 플랫폼", // SNS 공유 시 설명
-    url: "https://cshoon95-rang-art.vercel.app/", // 실제 서비스 URL로 변경해주세요
+    title: "랑아트 미술학원 😊", // 카톡 등 SNS 공유 시 제목
+    description: "랑아트 미술학원만을 위한 올인원 관리 플랫폼", // SNS 공유 시 설명
+    url: "https://cshoon95-rang-art.vercel.app", // 실제 서비스 URL로 변경해주세요
     siteName: "RANG ART",
     images: [
       {
-        url: "/icon.png", // 카톡 공유 시 보여질 이미지 경로 (public 폴더 기준)
+        url: "https://cshoon95-rang-art.vercel.app/icon.png", // 카톡 공유 시 보여질 이미지 경로 (public 폴더 기준)
         width: 800, // 권장 크기 (카톡 기준 800x400 또는 1:1 비율)
         height: 800,
         alt: "RANG ART Logo",
@@ -33,13 +33,6 @@ export const metadata: Metadata = {
     ],
     locale: "ko_KR",
     type: "website",
-  },
-  // 트위터 카드 설정 (선택 사항)
-  twitter: {
-    card: "summary_large_image",
-    title: "RANG ART - 아이들의 행복한 창작놀이터",
-    description: "선생님과 원장님을 위한 올인원 미술학원 관리 플랫폼",
-    images: ["/icon.png"], // 트위터 공유 시 이미지
   },
 };
 
