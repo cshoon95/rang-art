018cdcd43af6c439e25746970b66d1f61fae21dd
조수훈 <cshoon80@gmail.com>
2025-12-19 10:06:57 +0900

    Feat Prod에서 로그 제거

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compiler: {
+    // 운영 환경(production)에서만 모든 console.* 로그를 제거합니다.
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
